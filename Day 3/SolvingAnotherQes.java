ae26ba04b97d58cf18ae22d00f76938515d0336c
Anurag Singh <147307671+Anurag-Singh-0@users.noreply.github.com>
2025-09-18 17:08:26 +0530

    Solving some practice question

---
+++ Day 3/SolvingAnotherQes.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+// Sum of Even add Odd Number
+public class SolvingAnotherQes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter any even or odd number for sum : ");
+        int num = sc.nextInt();
+
+        // int evenSum = 0;
+        // int oddSum = 0;
+
+        // for (int i = 1; i <= num; i++) {
+        // if (i % 2 == 0) {
+        // evenSum = evenSum + i;
+        // } else {
+        // oddSum += i;
+        // }
+        // }
+
+        // System.out.println("Sum of even number : " + evenSum);
+        // System.out.println("Sum of odd number : " + oddSum);
+
+        // Factorial of a number
+        /*
+         * int factorial = 1;
+         * 
+         * for (int i = 1; i <= num; i++) {
+         * factorial = factorial * i;
+         * }
+         * System.out.println(factorial);
+         */
+
+        // Table
+        /*
+         * for (int i = 1; i <= 10; i++) {
+         * System.out.println(num + " * " + i + " = "+ (num * i));
+         * }
+         */
+    }
+
+}
