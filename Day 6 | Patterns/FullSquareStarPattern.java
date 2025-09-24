ec9967db35fe16c0dd3d95a4ca35010652d79e28
Anurag Singh <147307671+Anurag-Singh-0@users.noreply.github.com>
2025-09-25 00:32:36 +0530

    Completed Full star pattern program

--- Day 6 | Patterns/FullSquareStarPattern.java
+++ Day 6 | Patterns/FullSquareStarPattern.java
@@ -1,6 +1,13 @@
 public class FullSquareStarPattern {
 
     public static void main(String[] args) {
-        
+        int num = 5;
+
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <= num; j++) {
+                System.out.print(" * ");
+            }
+            System.out.println(" ");
+        }
     }
 }
