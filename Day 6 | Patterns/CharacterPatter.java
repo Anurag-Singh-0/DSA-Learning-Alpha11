d92e460620427f83c50d14bf34e1626245d16f3e
Anurag Singh <147307671+Anurag-Singh-0@users.noreply.github.com>
2025-09-27 00:39:49 +0530

    Completed the program

--- Day 6 | Patterns/CharacterPatter.java
+++ Day 6 | Patterns/CharacterPatter.java
@@ -1,5 +1,30 @@
 public class CharacterPatter {
     public static void main(String[] args) {
-        
+        char ch = 'A';
+        int num = 10;
+
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+        }
     }
 }
+
+// ✴️ Important ✴️
+
+/*
+ * char ch = 'A'; → You start with the letter A.
+ * (Remember: in Java, characters follow ASCII/Unicode order, so 'A' + 1 = 'B',
+ * 'B' + 1 = 'C', etc.)
+ */
+
+// Output
+
+// A
+// BC
+// DEF
+// GHIJ
+// KLMNO
