5a09f1606b62e2ad48c81e45a03ea968ac697dc0
Anurag Singh <147307671+Anurag-Singh-0@users.noreply.github.com>
2025-09-27 00:10:22 +0530

    Program completed

--- Day 6 | Patterns/NumberPattern.java
+++ Day 6 | Patterns/NumberPattern.java
@@ -1,11 +1,22 @@
 public class NumberPattern {
     public static void main(String[] args) {
-       int num  = 5; 
+        int num = 5;
 
-       for(int i = 1; i<= num; i++){
-         for (int j = 1; j <= num; j++) {
-                    
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
         }
-       }
     }
 }
+
+
+// Output
+/* 
+1
+12
+123
+1234
+12345
+ */
