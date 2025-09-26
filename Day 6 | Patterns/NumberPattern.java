fae041fcfb8092aee2b96b68122bef45ada8a6ef
Anurag Singh <147307671+Anurag-Singh-0@users.noreply.github.com>
2025-09-27 00:04:34 +0530

    writing inner loop

--- Day 6 | Patterns/NumberPattern.java
+++ Day 6 | Patterns/NumberPattern.java
@@ -3,7 +3,9 @@ public class NumberPattern {
        int num  = 5; 
 
        for(int i = 1; i<= num; i++){
-        
+         for (int j = 1; j <= num; j++) {
+                    
+        }
        }
     }
 }
