0e03660461f8ccffe87a70712460902be479937a
Anurag Singh <147307671+Anurag-Singh-0@users.noreply.github.com>
2025-09-28 00:43:31 +0530

    Writing Factorial program

--- Day 5 | Function/FunctionRevision.java
+++ Day 5 | Function/FunctionRevision.java
@@ -2,19 +2,30 @@ import java.util.Arrays;
 
 public class FunctionRevision {
 
-    public static void swap(int a, int b) {
-    int temp = 0;
+    // public static void swap(int a, int b) {
+    // int temp = 0;
 
-    temp = a;
-    a = b;
-    b = temp;
+    // temp = a;
+    // a = b;
+    // b = temp;
 
-    System.out.println("a = " + a);
-    System.out.println("b = " + b);
+    // System.out.println("a = " + a);
+    // System.out.println("b = " + b);
+    // }
+
+    public static void factorial(int num) {
+        int fac = 1;
+
+        for (int i = 1; i <= num; i++) {
+            fac = fac * i;
+        }
+
+        System.out.println("Factorial of " + num + " is = " + fac);
     }
 
     public static void main(String[] args) {
-        swap(5, 7);
+        // swap(5, 7);
+        factorial(5);
 
     }
 }
